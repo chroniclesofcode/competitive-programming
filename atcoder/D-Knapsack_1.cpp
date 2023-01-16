d4e2658b7f6924bc979ee55835013ccfdcae5e83
Donald Tang <donald.tang.email@googlemail.com>
2023-01-16 19:45:57 +1100

    atcoder - D-Knapsack_1.cpp

---
+++ atcoder/D-Knapsack_1.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAINRET(x) in##x
+#define LL long long
+
+void solve();
+
+MAINRET(t) main(void) {
+    std::cin.tie(nullptr);
+    std::cin.sync_with_stdio(false);
+
+        solve();
+}
+
+#define INF numeric_limits<LL>::max() / 2
+const LL MX = 1e5+5;
+//const LL MOD = 1e7;
+
+LL dp[101][MX];
+pair<int, int> a[101];
+/*
+Knapsack:
+    
+*/
+
+void solve() {
+    LL n, w;
+    cin >> n >> w;
+    for (LL i = 1; i <= n; i++) {
+        LL wi, vi;
+        cin >> wi >> vi;
+        a[i] = { wi, vi };
+    }
+
+    /*
+        Given a bag of weight j, what is the max
+        value we can hold with all items up to the ith?
+        */
+    for (LL i = 1; i <= n; i++) {
+        for (LL j = 1; j <= w; j++) {
+            LL k = max(j-a[i].first, (LL)0);
+            LL val = j-a[i].first < 0 ? 0 : a[i].second;
+            dp[i][j] = max(dp[i-1][j], dp[i-1][k] + val);
+        }
+    }
+
+    /*
+    for (LL i = 1; i <= n; i++) {
+        cout << i << ": ";
+        for (LL j = 1; j <= w; j++) {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+    */
+    cout << dp[n][w] << '\n';
+
+}
+
+
+
+
+
--- prev.cpp
+++ prev.cpp
@@ -10,66 +10,29 @@ void solve();
 MAINRET(t) main(void) {
     std::cin.tie(nullptr);
     std::cin.sync_with_stdio(false);
-    LL t;
-    cin >> t;
-    while (t--)
+
         solve();
 }
 
 #define INF numeric_limits<LL>::max() / 2
-const LL MX = 10;
+const LL MX = 5 * 1e5;
 //const LL MOD = 1e7;
 
-int n, m;
-int b[MX][MX];
-int r[MX];
-int c[MX];
+LL n, w;
+LL a[MX][2]; 
+LL dp[110][MX];
 /*
 
 */
 
-vector<pair<int, int>> rooks;
-
 void solve() {
-    memset(b, 0, sizeof b);
-    memset(r, 0, sizeof r);
-    memset(c, 0, sizeof c);
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int x, y;
-        cin >> x >> y;
-        x--;y--;
-        b[x][y] = 1;
-        r[x]++;
-        c[y]++;
-        rooks.push_back({x, y});
-    }
-    bool res = false;
+    cin >> n >> w;
 
-    for (auto p : rooks) {
-        int x = p.first;
-        int y = p.second;
-        r[x]--;
-        c[y]--;
-        b[x][y] = 0;
-        for (int i = 0; i < n; i++) {
-            if (res) break;
-            for (int j = 0; j < n; j++) {
-                if (r[i] == 0 && c[i] == 0 && i != x && j != y && !b[x][y]) {
-                    res = true;
-                }
-            }
-        }
-        if (res) break;
-        r[x]++;
-        c[y]++;
-        b[x][y] = 1;
-    }
-
-    if (res) {
-        cout << "YES\n";
-    } else {
-        cout << "NO\n";
+    for (LL i = 0; i < n; i++) {
+        int u, v;
+        cin >> u >> v;
+        a[i][0] = u; // weight
+        a[i][1] = v; // value
     }
 }
 
