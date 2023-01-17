16a99ac7953e5ccb0bd48ccbe8e28f08f5af1e6a
Donald Tang <donald.tang.email@googlemail.com>
2023-01-17 20:37:20 +1100

    atcoder - E-Knapsack_2.cpp

--- prev.cpp
+++ prev.cpp
@@ -15,26 +15,50 @@ MAINRET(t) main(void) {
 }
 
 #define INF numeric_limits<LL>::max() / 2
-const LL MX = 5 * 1e5;
+const LL MX = 1e5+5;
 //const LL MOD = 1e7;
 
 LL n, w;
-LL a[MX][2]; 
-LL dp[110][MX];
+LL dp[101][MX];
+pair<LL, LL> a[101];
 /*
-
+Knapsack:
+    
 */
 
 void solve() {
     cin >> n >> w;
-
-    for (LL i = 0; i < n; i++) {
-        int u, v;
-        cin >> u >> v;
-        a[i][0] = u; // weight
-        a[i][1] = v; // value
+    for (LL i = 1; i <= n; i++) {
+        LL wi, vi;
+        cin >> wi >> vi;
+        a[i] = { wi, vi };
     }
+
+    /*
+        Given a bag of weight j, what is the max
+        value we can hold with all items up to the ith?
+        */
+    for (LL i = 1; i <= n; i++) {
+        for (LL j = 1; j <= w; j++) {
+            LL k = max(j-a[i].first, (LL)0);
+            LL val = j-a[i].first < 0 ? 0 : a[i].second;
+            dp[i][j] = max(dp[i-1][j], dp[i-1][k] + val);
+        }
+    }
+
+    /*
+    for (LL i = 1; i <= n; i++) {
+        cout << i << ": ";
+        for (LL j = 1; j <= w; j++) {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+    */
+    cout << dp[n][w] << '\n';
+
 }
+
 
 
 
