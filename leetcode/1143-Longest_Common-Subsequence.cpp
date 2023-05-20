b1450afff8945614dc6db932e5af69fdbacd4315
Donald Tang <donald.tang.email@googlemail.com>
2023-05-20 18:18:22 +1000

    leetcode - 1143-Longest_Common-Subsequence.cpp

---
+++ leetcode/1143-Longest_Common-Subsequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int ans = 0;
+        int dp[1001][1001];
+
+        for (int i = 0; i < text1.length(); i++) {
+            for (int j = 0; j < text2.length(); j++) {
+                if (text1[i] != text2[j]) {
+                    if (i == 0 && j == 0) {
+                        dp[i][j] = 0;
+                    } else if (i == 0) {
+                        dp[i][j] = dp[i][j-1];
+                    } else if (j == 0) { 
+                        dp[i][j] = dp[i-1][j];
+                    } else {
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    }
+                } else {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = 1 + dp[i-1][j-1];
+                    }
+                }
+                ans = max(dp[i][j], ans);
+            }
+        }
+        return ans;
+    }
+};
