7c63ad359430d153718ddae4450bef53d1f6a762
Donald Tang <donald.tang.email@googlemail.com>
2023-05-20 17:56:50 +1000

    Changed formatting of leetcode

--- leetcode/718-Maximum_Length_Of_Repeated_Subarray.cpp
+++ leetcode/718-Maximum_Length_Of_Repeated_Subarray.cpp
@@ -1,33 +1,3 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define MAINRET(x) in##x
-#define LL long long
-
-void solve();
-
-MAINRET(t) main(void) {
-    std::cin.tie(nullptr);
-    std::cin.sync_with_stdio(false);
-
-        solve();
-}
-
-#define INF numeric_limits<LL>::max() / 2
-const LL MX = 5 * 1e6;
-//const LL MOD = 1e7;
-
-int n;
-
-/*
-
-*/
-
-void solve() {
-
-}
-
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
--- prev.cpp
+++ prev.cpp
@@ -15,54 +15,18 @@ MAINRET(t) main(void) {
 }
 
 #define INF numeric_limits<LL>::max() / 2
-const LL MX = 1e5+5;
+const LL MX = 5 * 1e6;
 //const LL MOD = 1e7;
 
-LL n, w;
-//LL dp[101][MX];
-pair<LL, LL> a[101];
-vector<LL> dp(MX, INF);
+int n;
+
 /*
-Knapsack:
-    
+
 */
 
 void solve() {
-    cin >> n >> w;
-    LL tot = 0;
-    for (LL i = 1; i <= n; i++) {
-        LL wi, vi;
-        cin >> wi >> vi;
-        a[i] = { wi, vi };
-        tot += vi;
-    }
-
-    /*
-       DP
-       ConstraLLs means we cannot iterate on
-       weight. Must iterate on possible values
-       Use DP to find out which values are possible
-       For every item up to i, for every j value
-       calculate the minimum weight required
-       to achieve that certain value
-    */
-    dp[0] = 0;
-    for (LL i = 1; i <= n; i++) {
-        for (LL j = tot - a[i].second; j >= 0; j--) {
-            dp[j+a[i].second] = min(dp[j+a[i].second], dp[j] + a[i].first);
-        }
-    }
-    LL ret = 0;
-    for (LL i = tot; i >= 0; i--) {
-        if (dp[i] <= w) {
-            ret = i;
-            break;
-        }
-    }
-    cout << ret << '\n';
 
 }
-
 
 
 
