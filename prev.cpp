ba9e7434c4982f7c5b1285f71388160014636943
Donald Tang <donald.tang.email@googlemail.com>
2023-07-22 12:25:36 +1000

    codeforces - 1253D-Harmonious_Graph.cpp

--- prev.cpp
+++ prev.cpp
@@ -1,32 +1,100 @@
-class Solution {
-public:
-    vector<int> goodIndices(vector<int>& nums, int k) {
-        const int MX = nums.size()+5;
-        int n = nums.size();
-        int inc[MX], dec[MX];
-        dec[0] = 1;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] <= nums[i-1]) {
-                dec[i] = dec[i-1] + 1;
-            } else {
-                dec[i] = 1;
-            }
-        }
-        inc[n-1] = 1;
-        for (int i = n-2; i >= 0; i--) {
-            if (nums[i] <= nums[i+1]) {
-                inc[i] = inc[i+1] + 1;
-            } else {
-                inc[i] = 1;
-            }
-        }
+#include <bits/stdc++.h>
 
-        vector<int> ans;
-        for (int i = k; i < n-k; i++) {
-            if (dec[i-1] >= k && inc[i+1] >= k) {
-                ans.push_back(i);
+using namespace std;
+
+#define MAINRET(x) in##x
+#define LL long long
+
+void solve();
+
+MAINRET(t) main(void) {
+    std::cin.tie(nullptr);
+    std::cin.sync_with_stdio(false);
+
+        solve();
+}
+
+#define INF numeric_limits<LL>::max() / 2
+#define NINF -INF
+
+const LL MX = 5 * 1e5;
+//const LL MOD = 1e7;
+
+int n, m, dp[MX][26];
+bool vis[MX], active[MX], seen[MX];
+string s;
+vector<int> order, adj[MX], rev[MX];
+/*
+
+*/
+
+bool hasCycle(int u) {
+    if (vis[u]) return false;
+    vis[u] = true;
+    active[u] = true;
+
+    for (int v : adj[u]) {
+        if (active[v] || hasCycle(v)) {
+            return true;
+        }
+    }
+    active[u] = false;
+    return false;
+}
+
+void dfs(int u) {
+    if (seen[u]) return;
+    seen[u] = 1;
+    for (int v : adj[u]) {
+        if (!seen[v]) dfs(v);
+    }
+    order.push_back(u);
+    return;
+}
+
+void solve() {
+    cin >> n >> m;
+    cin >> s;
+    int x, y;
+    for (int i = 0; i < m; i++) {
+        cin >> x >> y;
+        x--; y--;
+        adj[x].push_back(y);
+        rev[y].push_back(x);
+    }
+    for (int i = 0; i < n; i++) {
+        if (!vis[i] && hasCycle(i)) {
+            cout << -1 << '\n';
+            return;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (!seen[i]) {
+            dfs(i);
+        }
+    }
+    int ans = 0;
+    reverse(order.begin(), order.end());
+    for (int u : order) {
+        int ch = s[u] - 'a';
+        for (int v : rev[u]) {
+            for (int i = 0; i < 26; i++) {
+                dp[u][i] = max(dp[u][i], dp[v][i]);
             }
         }
-        return ans;
+        dp[u][ch]++;
+        ans = max(ans, dp[u][ch]);
+        /*
+        cout << "\nafter " << u+1 << ':' << "\n";
+        for (int i = 0; i < 26; i++) {
+            cout << (char)(i + 'a') << ": " << dp[u][i] << ' ';
+        }
+        */
     }
-};
+    cout << ans << '\n';
+}
+
+
+
+
+
