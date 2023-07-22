1117003d765016607330ff64d8331c69ffd05ec7
Donald Tang <donald.tang.email@googlemail.com>
2023-07-22 12:56:06 +1000

    codeforces - 1209D-Cows_And_Snacks.cpp

--- codeforces/1209D-Cows_And_Snacks.cpp
+++ codeforces/1209D-Cows_And_Snacks.cpp
@@ -20,27 +20,20 @@ MAINRET(t) main(void) {
 const LL MX = 5 * 1e6;
 //const LL MOD = 1e7;
 
-int n, m, grp[MX], sz[MX], reach[MX];
-bool vis[MX];
+int n, k, sz[MX], grp[MX], vis[MX];
 vector<int> adj[MX];
 
 int Find(int a) {
-    if (grp[a] == a) {
-        return a;
-    }
+    if (a == grp[a]) return a;
     return grp[a] = Find(grp[a]);
 }
-
 void Union(int a, int b) {
     a = Find(a);
     b = Find(b);
     if (a != b) {
-        if (sz[a] < sz[b]) {
-            swap(a, b);
-        }
+        if (sz[a] < sz[b]) swap(a, b);
         grp[b] = a;
         sz[a] += sz[b];
-        reach[a] = max(reach[a], reach[b]);
     }
 }
 
@@ -50,21 +43,20 @@ void Union(int a, int b) {
 
 void dfs(int u) {
     if (vis[u]) return;
-    reach[grp[u]] = max(reach[grp[u]], u);
     vis[u] = 1;
+
     for (int v : adj[u]) {
         if (!vis[v]) {
             Union(u, v);
             dfs(v);
         }
     }
-    return;
 }
 
 void solve() {
-    cin >> n >> m;
+    cin >> n >> k;
     int x, y;
-    for (int i = 0 ; i < m; i++) {
+    for (int i = 0; i < k; i++) {
         cin >> x >> y;
         x--; y--;
         adj[x].push_back(y);
@@ -72,35 +64,17 @@ void solve() {
     }
     for (int i = 0; i < n; i++) {
         grp[i] = i;
-        reach[i] = i;
+        sz[i] = 1;
     }
+    
+    int ans = 0;
     for (int i = 0; i < n; i++) {
         if (!vis[i]) {
             dfs(i);
+            ans += sz[Find(i)] - 1;
         }
     }
-    unordered_map<int, int> seen;
-    seen[grp[0]] = 1;
-    int ans = 0;
-    int curr = reach[grp[0]];
-    /*
-    for (int i = 0; i < n; i++) {
-        cout << "grp["<<i<<"]: "<<grp[i]<<","<<reach[i]<< '\n';
-    }
-    */
-    for (int i = 1; i < n; i++) {
-        if (i <= curr) {
-            if (!seen[grp[i]]) {
-                ans++;
-                seen[grp[i]] = 1;
-            }
-            curr = max(curr, reach[grp[i]]);
-        } else {
-            seen[grp[i]] = 1;
-            curr = reach[grp[i]];
-        }
-    }
-    cout << ans << '\n';
+    cout << k - ans << '\n';
 }
 
 
