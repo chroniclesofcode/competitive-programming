788d877c0257b1b89b36f934db621442dcea8955
Donald Tang <donald.tang.email@googlemail.com>
2023-07-29 19:44:23 +1000

    codeforces - 999E-Reachability_from_the_Capital.cpp

--- prev.cpp
+++ prev.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 #define MAINRET(x) in##x
-#define LL int
+#define LL long long
+
 void solve();
 
 MAINRET(t) main(void) {
@@ -15,72 +16,65 @@ MAINRET(t) main(void) {
 
 #define INF numeric_limits<LL>::max() / 2
 #define NINF -INF
+#define arr array<LL,2>
 
-const LL MX = 5 * 110;
+const LL MX = 5 * 1e6;
 //const LL MOD = 1e7;
 
-int n, a[MX], dp[MX][MX], pali[MX][MX];
-
+LL n, vis[MX], seen[MX], dist[MX], ans = 0;
+vector<arr> adj[MX];
 /*
-    Range dp dp[i][j] is time it takes to destroy
-    i..j, on each iteration, check if the new value
-    creates new palindromes -> O(n^3)
+
 */
 
-void debug() {
-    cout << "\nDEBUG\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << pali[i][j] << ' ';
+LL dfs(LL u) {
+    if (vis[u]) return 0;
+    vis[u] = 1;
+
+    LL ct = 0;
+
+    for (arr v : adj[u]) {
+        if (!vis[v[0]]) {
+            ct += dfs(v[0]) + 2*v[1];
         }
-        cout << endl;
     }
-    cout << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << dp[i][j] << ' ';
+    return ct;
+}
+
+void dfs2(LL u, LL dis) {
+    if (seen[u]) {
+        return;
+    }
+    seen[u] = 1;
+    dist[u] = dis;
+    LL ct = 0;
+    for (arr v : adj[u]) {
+        if (!seen[v[0]]) {
+            dfs2(v[0], dis+v[1]);
+            ct++;
         }
-        cout << endl;
     }
-    cout << endl;
+    if (ct == 0 && u != 0) {
+        ans = max(ans, dist[u]);
+    }
+    return;
 }
 
 void solve() {
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
+    LL x,y,z;
+    for (LL i = 0; i < n-1; i++) {
+        cin >> x >> y >> z;
+        x--; y--;
+        adj[x].push_back({y,z});
+        adj[y].push_back({x,z});
     }
-    for (int i = n-1; i >= 0; i--) {
-        for (int j = i; j < n; j++) {
-            dp[i][j] = INF;
-            if (i == j) {
-                pali[i][j] = 1;
-            } else if (a[i] == a[j]) {
-                if (j-i <= 1) {
-                    pali[i][j] = 1;
-                } else {
-                    pali[i][j] = pali[i+1][j-1];
-                }
-            }
-        }
-    }
-    for (int i = n-1; i >= 0; i--) {
-        for (int j = i; j < n; j++) {
-            dp[i][j] = min(dp[i][j], 1 + dp[i+1][j]);
-            dp[i][j] = min(dp[i][j], 1 + dp[i][j-1]);
-            for (int k = i; k <= j; k++) {
-                if (pali[i][k]) {
-                    dp[i][j] = min(dp[i][j], 1 + dp[k+1][j]);
-                }
-                if (a[i] == a[k] && k - i <= 1) {
-                    dp[i][j] = min(dp[i][j], 1+ dp[k+1][j]);
-                } else if (a[i] == a[k]) {
-                    dp[i][j] = min(dp[i][j], dp[i+1][k-1]+dp[k+1][j]);
-                }
-            }
-        }
-    }
-    //debug();
-    cout << dp[0][n-1] << '\n';
+    LL tot = dfs(0);
+    dfs2(0,0);
+    cout << tot - ans << endl;
 }
+
+
+
+
 
