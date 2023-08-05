df571c5f06ed0064c17ad3a780cc1210a7d476fa
Donald Tang <donald.tang.email@googlemail.com>
2023-08-05 17:11:35 +1000

    codeforces - 1328D-Carousel.cpp

--- prev.cpp
+++ prev.cpp
@@ -17,41 +17,50 @@ MAINRET(t) main(void) {
 #define INF numeric_limits<LL>::max() / 2
 #define NINF -INF
 
-const LL MX = 1e8;
+const LL MX = 5 * 1e3;
 //const LL MOD = 1e7;
 
-LL n1, n2, k1, k2;
-// Num of ways to arrange with these limitations
-LL dp[101][101][11][11];
+LL n, dist[MX][MX], p;
 
 /*
 
 */
 
 void solve() {
-    cin >> n1 >> n2 >> k1 >> k2;
-    for (int i = 0; i <= n1; i++) {
-        for (int j = 0; j <= n2; j++) {
-            for (int k = 0; k <= k1; k++) {
-                for (int l = 0; l <= k2; l++) {
-                    int ans = 0;
-                    if (i + j == 0) {
-                        ans = 1;
-                    } else {
-                        if (i > 0 && k > 0) {
-                            ans += dp[i-1][j][k-1][k2];
-                        }
-                        if (j > 0 && l > 0) {
-                            ans += dp[i][j-1][k1][l-1];
-                        }
-                        ans %= MX;
-                    }
-                    dp[i][j][k][l] = ans;
-                }
-            }
+    cin >> n;
+    LL x;
+    for (LL i = 0; i < n; i++) {
+        for (LL j = 0; j < n; j++) {
+            cin >> x;
+            dist[i][j] = x;
         }
     }
-    cout << dp[n1][n2][k1][k2] << '\n';
+    cin >> p;
+    LL y, z;
+    for (LL g = 0; g < p; g++) {
+        cin >> x >> y >> z;
+        x--; y--;
+        dist[x][y] = min(dist[x][y], z);
+        dist[y][x] = dist[x][y];
+        LL ans = 0;
+        for (LL i = 0; i < n; i++) {
+            for (LL j = 0; j < n; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][x]+z+dist[y][j]);
+                dist[i][j] = min(dist[i][j], dist[i][y]+z+dist[x][j]);
+            }
+        }
+        for (LL i = 0; i < n; i++) {
+            for (LL j = 0; j < i; j++) {
+                ans += dist[i][j];
+            }
+        }
+
+        cout << ans << ' ';
+    }
+    cout << endl;
+
+    
+
 }
 
 
