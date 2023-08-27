77ffd456952c8da0aad468c908101bde92aa292a
Donald Tang <donald.tang.email@googlemail.com>
2023-08-27 22:38:56 +1000

    leetcode - 199-Binary_Tree_Right_Side_View.cpp

--- prev.cpp
+++ prev.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums, int target) {
-        unordered_map<int, int> m;
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < 31; j++) {
-                if (nums[i] & (1 << j)) {
-                    m[j]++;
-                    break;
-                }
+    int partition(vector<int> &nums, int l, int r) {
+        int piv = nums[r];
+        int j = l-1;
+        for (int i = l; i < r; i++) {
+            if (nums[i] < piv) {
+                j++;
+                swap(nums[i], nums[j]);
             }
         }
-        int ans = 0;
-        int ct = 0;
-        for (int i = 0; i < 31; i++) {
-            if (target & (1 << i)) {
-                int j = i;
-                while (j < 31 && m[j] == 0) {
-                    j++;
-                }
-                if (j == 31) return -1;
-                while (j != i) {
-                    m[j]--;
-                    m[j-1] += 2;
-                    ans++;
-                    j--;
-                }
-                m[i]--;
-                ct += (1 << i);
-            }
-            m[i+1] += m[i] / 2;
-            if (ct == target) {
-                break;
-            }
-        }
-        return ans;
+        swap(nums[j+1], nums[r]);
+        return j+1;
+    }
+
+    void qs(vector<int> &nums, int l, int r) {
+        if (l >= r) return;
+        int p = partition(nums, l, r);
+        qs(nums, l, p-1);
+        qs(nums, p+1, r);
+    }
+
+    void sortColors(vector<int>& nums) {
+        qs(nums, 0, nums.size() - 1);
     }
 };
