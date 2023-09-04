958dac306e60e89981a7edad33220b4d7a7a528b
Donald Tang <donald.tang.email@googlemail.com>
2023-09-04 20:43:34 +1000

    codeforces - 1858B-The_Walkway.cpp

--- prev.cpp
+++ prev.cpp
@@ -21,36 +21,13 @@ const LL MX = 5 * 1e5;
 //const LL MOD = 1e7;
 
 int n;
-vector<int> adj[MX];
-int ans = 0;
-bool vis[MX];
 
 /*
 
 */
 
-void dfs(int u, int prev) {
-    for (int v : adj[u]) {
-        if (v != prev) {
-            dfs(v, u);
-        }
-    }
-    if (prev != 0 && !vis[u] && !vis[prev]) {
-        vis[u] = 1; vis[prev] = 1;
-        ans++;
-    }
-}
-
 void solve() {
-    cin >> n;
-    int a, b;
-    for (int i = 0; i < n-1; i++) {
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    dfs(1, 0);
-    cout << ans << endl;
+
 }
 
 
