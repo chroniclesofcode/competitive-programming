5dbc4f7a95a0aed41d9a89a1c711509dffc4a4ad
Donald Tang <donald.tang.email@googlemail.com>
2023-12-17 00:15:10 +1100

    codeforces - 1839C-Insert_Zero_and_Invert_Prefix.cpp

--- prev.cpp
+++ prev.cpp
@@ -20,24 +20,36 @@ MAINRET(t) main(void) {
 #define INF numeric_limits<LL>::max() / 2
 const LL MX = 5 * 1e6;
 //const LL MOD = 1e7;
-
-int n, k;
-
+#define arr array<LL,2>
+int n;
 /*
 
 */
 
 void solve() {
-    cin >> n >> k;
-    vector<int> dp(n+1, 0);
-    int ans = 0;
-    for (int i = 1; i <= n; i += k) {
-        ans++;
-        dp[i] = 1;
+    cin >> n;
+    vector<arr> v;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        v.push_back({a, b});
     }
-    if (dp[n] != 1) cout << ans+1;
-    else cout << ans;
-    cout << '\n';
+   
+    sort(v.begin(), v.end(), greater<arr>());
+    LL prev = -1;
+    int ct = 0;
+    LL ans = 0;
+    for (auto e : v) {
+        if (e[0] == prev && ct == prev) continue;
+        if (e[0] != prev) {
+            prev = e[0];
+            ct = 0;
+        }
+        ans += e[1];
+        ct++;
+        prev = e[0];
+    }
+    cout << ans << '\n';
 }
 
 
