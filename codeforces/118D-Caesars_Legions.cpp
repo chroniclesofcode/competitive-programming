a155e7040dfed064a8846d5211aee41831771a5d
Donald Tang <donald.tang.email@googlemail.com>
2023-12-30 18:40:46 +1100

    codeforces - 118D-Caesars_Legions.cpp

--- codeforces/118D-Caesars_Legions.cpp
+++ codeforces/118D-Caesars_Legions.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 #define MAINRET(x) in##x
+#define what_is(x) cout << #x << " is " << x << endl;
 #define LL long long
+#define arr array<int,2>
 
 void solve();
 
@@ -14,47 +16,40 @@ MAINRET(t) main(void) {
         solve();
 }
 
-#define INF numeric_limits<LL>::max() / 2
-#define NINF -INF
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr int NINF = -INF;
+constexpr LL MX = 3 * 1e5;
+constexpr int MD = (int)1e8;
 
-const LL MX = 1e8;
-//const LL MOD = 1e7;
-
-LL n1, n2, k1, k2;
-// Num of ways to arrange with these limitations
-LL dp[101][101][11][11];
+int n1, n2, k1, k2;
+int dp[101][101][2];
+void solve() {
+    cin >> n1 >> n2 >> k1 >> k2;
+    for (int i = 0; i <= k1; i++) {
+        dp[i][0][0] = 1;
+    }
+    for (int i = 0; i <= k2; i++) {
+        dp[0][i][1] = 1;
+    }
+    for (int i = 1; i <= n1; i++) {
+        for (int j = 1; j <= n2; j++) {
+            // 0 is footmen, 1 is horsemen
+            for (int k = 1; k <= min(i, k1); k++) {
+                dp[i][j][0] += dp[i-k][j][1];
+                dp[i][j][0] %= MD;
+            }
+            for (int k = 1; k <= min(j, k2); k++) {
+                dp[i][j][1] += dp[i][j-k][0];
+                dp[i][j][1] %= MD;
+            }
+        }
+    }
+    cout << (dp[n1][n2][0] + dp[n1][n2][1])%MD << '\n';
+}
 
 /*
 
 */
-
-void solve() {
-    cin >> n1 >> n2 >> k1 >> k2;
-    for (int i = 0; i <= n1; i++) {
-        for (int j = 0; j <= n2; j++) {
-            for (int k = 0; k <= k1; k++) {
-                for (int l = 0; l <= k2; l++) {
-                    int ans = 0;
-                    if (i + j == 0) {
-                        ans = 1;
-                    } else {
-                        if (i > 0 && k > 0) {
-                            ans += dp[i-1][j][k-1][k2];
-                        }
-                        if (j > 0 && l > 0) {
-                            ans += dp[i][j-1][k1][l-1];
-                        }
-                        ans %= MX;
-                    }
-                    dp[i][j][k][l] = ans;
-                }
-            }
-        }
-    }
-    cout << dp[n1][n2][k1][k2] << '\n';
-}
-
-
 
 
 
