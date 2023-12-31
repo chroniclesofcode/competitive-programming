37ba6a8fdcda3d1680a9c785b77ffa76daeca1b8
Donald Tang <donald.tang.email@googlemail.com>
2023-12-31 15:25:53 +1100

    Accident

--- prev.cpp
+++ prev.cpp
@@ -1,61 +1,37 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define MAINRET(x) in##x
-#define what_is(x) cout << #x << " is " << x << endl;
-#define LL long long
-#define arr array<LL,2>
-
-void solve();
-
-MAINRET(t) main(void) {
-    std::cin.tie(nullptr);
-    std::cin.sync_with_stdio(false);
-
-        solve();
-}
-
-constexpr LL INF = std::numeric_limits<LL>::max() / 2;
-constexpr LL NINF = -INF;
-constexpr LL MX = 3 * 1e5;
-constexpr LL MD = (LL)1e9 + 7;
-
-LL n, m, k;
-
-
-void solve() {
-    cin >> n >> k;
-    vector<LL> a(n);
-    for (LL i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    sort(a.begin(), a.end());
-    vector<LL> pref;
-    LL p = 0;
-    for (LL i = 0; i < n; i++) {
-        p += a[i];
-        pref.push_back(p);
-    }
-    LL ans = 1;
-    LL ret = a[0];
-    LL j = 0;
-    LL sum = 0;
-    for (LL i = 0; i < n; i++) {
-        while (j < i && a[i]*(i-j+1) - (pref[i]- (j == 0 ? 0 : pref[j-1])) > k) {
-            j++;
+class Solution {
+public:
+    int maximumLength(string s) {
+        cout << s << '\n';
+        int n = s.size();
+        vector<int> ct(26, 0);
+        int longest = 1;
+        map<int, int, greater<int>> occ[26];
+        ct[s[0]-'a']++;
+        occ[s[0]-'a'][1]++;
+        for (int i = 1; i < n; i++) {
+            int c = s[i] - 'a';
+            if (s[i] == s[i-1]) {
+                longest++;
+            } else {
+                longest = 1;
+            }
+            ct[c] = max(ct[c], longest);
+            occ[c][longest]++;
         }
-        if (i-j+1 > ans) {
-            ans = i-j+1;
-            ret = a[i];
+        int ans = -1;
+        for (int i = 0; i < 26; i++) {
+            auto top = occ[i].begin();
+            int tmp = top->first >= 3 ? top->first - 2 : -1;
+            int cumu = 0;
+            for (auto& it : occ[i]) {
+                cumu += it.second;
+                if (cumu >= 3) {
+                    tmp = max(tmp, it.first);
+                    break;
+                }
+            }
+            ans = max(ans, tmp);
         }
+        return ans;
     }
-    cout << ans << ' ' << ret << '\n';
-}
-
-/*
-
-*/
-
-
-
+};
