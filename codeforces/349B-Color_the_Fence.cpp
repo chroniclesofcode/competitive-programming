ca2bdf35797689331786bc38b0f0c5c7e517a4f5
Donald Tang <donald.tang.email@googlemail.com>
2024-01-08 20:37:20 +1100

    codeforces - 349B-Color_the_Fence.cpp

---
+++ codeforces/349B-Color_the_Fence.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAINRET(x) in##x
+#define what_is(x) cout << #x << " is " << x << endl;
+#define LL long long
+#define arr array<int,2>
+
+void solve();
+
+MAINRET(t) main(void) {
+    std::cin.tie(nullptr);
+    std::cin.sync_with_stdio(false);
+
+        solve();
+}
+
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr int NINF = -INF;
+constexpr LL MX = 3 * 1e5;
+constexpr int MD = (int)1e9 + 7;
+
+int n, m, k, v;
+int cost[10];
+
+void solve() {
+    cin >> v;
+    int small = INF;
+    int sd = 0;
+    for (int i = 1; i <= 9; i++) {
+        cin >> cost[i];
+        if (cost[i] < small) {
+            small = cost[i];
+            sd = i;
+        }
+    }
+    if (small > v) {
+        cout << -1 << '\n';
+        return;
+    }
+    int numdig = v/small;
+    v -= small*numdig;
+    vector<int> ans(numdig, sd);
+    for (int i = 0; i < numdig; i++) {
+        for (int j = 9; j >= 1; j--) {
+            if (v + small - cost[j] >= 0) {
+                ans[i] = j;
+                v = v + small - cost[j];
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < numdig; i++) {
+        cout << ans[i];
+    }
+    cout << '\n';
+
+
+}
+
+/*
+   we want as many digits as possible -> use the smallest cost
+   to form the maximum amount of digits. Now, the number must
+   have that many digits. Then, just test all the cases.
+*/
+
+
+
--- prev.cpp
+++ prev.cpp
@@ -1,64 +1,19 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define MAINRET(x) in##x
-#define what_is(x) cout << #x << " is " << x << endl;
-#define LL long long
-#define arr array<int,2>
-
-void solve();
-
-MAINRET(t) main(void) {
-    std::cin.tie(nullptr);
-    std::cin.sync_with_stdio(false);
-
-        solve();
-}
-
-constexpr int INF = std::numeric_limits<int>::max() / 2;
-constexpr int NINF = -INF;
-constexpr LL MX = 1 * 1e5 + 1;
-constexpr int MD = (int)1e9 + 7;
-
-int n, m, k;
-vector<int> ans;
-vector<array<int,2>> adj[MX];
-int vis[MX];
-
-bool dfs(int u, int p, bool problem) {
-    bool flag = false;
-    for (auto e : adj[u]) {
-        int v = e[0], t = e[1];
-        if (v == p) continue;
-        flag |= dfs(v, u, t);
+class Solution {
+public:
+    int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
+        // If rook can capture in one without being blocked by the bishop
+        if (a == e && !(c == a && d > min(b,f) && d < max(b,f))) return 1;
+        if (b == f && !(d == b && c > min(a,e) && c < max(a,e))) return 1;
+        // If bishop can capture in one without being blocked by the rook
+        if (abs(c-e) == abs(d-f)) {
+            bool tq1 = e-c > 0, tq2 = f-d > 0;
+            bool tr1 = a-c > 0, tr2 = b-d > 0;
+            bool tr3 = a-e > 0, tr4 = b-f > 0;
+            if ((c == a || d == b) || !(abs(c-a)==abs(d-b))) return 1;
+            if (tq1 != tr1 || tq2 != tr2) return 1;
+            if (tq1 == tr3 && tq2 == tr4) return 1;
+        }
+        // Else, it will take 2 moves for rook to capture
+        return 2;
     }
-    if (!flag && problem) {
-        ans.push_back(u);
-        return true;
-    }
-    return flag;
-}
-
-void solve() {
-    cin >> n;
-    for (int i = 0; i < n-1; i++) {
-        int x,y,t; cin >> x >> y >> t;
-        x--; y--;
-        adj[x].push_back({y,t==2});
-        adj[y].push_back({x,t==2});
-    }
-    dfs(0, -1, false);
-    cout << ans.size() << '\n';
-    for (auto a : ans) {
-        cout << a+1 << ' ';
-    }
-    cout << '\n';
-}
-
-/*
-
-*/
-
-
-
+};
