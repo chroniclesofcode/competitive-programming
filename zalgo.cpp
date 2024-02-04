dd2786a1e0fac443a8eddf6b98b5e9b76e1ae1be
Donald Tang <donald.tang.email@googlemail.com>
2024-02-04 14:51:15 +1100

    leetcode - 100203-Minimum_Time_to_Revert_Word_to_Initial_S.cpp

--- zalgo.cpp
+++ zalgo.cpp
@@ -23,6 +23,24 @@ constexpr int MD = (int)1e9 + 7;
 
 int n, m, k;
 
+vector<int> zf(string &s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        if(i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if(i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+    return z;
+}
 
 void solve() {
 
