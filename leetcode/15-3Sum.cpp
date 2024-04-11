c32cfb0af723e8329c3a7b058017ef692287a8bf
Donald Tang <donald.tang.email@googlemail.com>
2024-04-11 19:10:17 +1000

    leetcode - 15-3Sum.cpp

--- leetcode/15-3Sum.cpp
+++ leetcode/15-3Sum.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        unordered_map<int, int> m;
-        unordered_set<int> s;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            m.clear();
-            s.clear();
-            for (int j = i+1; j < n; j++) {
-                if (s.count(nums[j])) continue;
-                if (i != j && m[-nums[i]-nums[j]] > 0 && !s.count(nums[j])) {
-                    ans.push_back({nums[i], nums[j], -nums[i]-nums[j]});
-                    s.insert(nums[j]);
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = 0; i < n-1; i++) {
+            int lo = i+1, hi = n-1;
+            int tot = nums[i];
+            while (lo < hi) {
+                int tmp = tot + nums[lo] + nums[hi];
+                if (tmp == 0) {
+                    ans.push_back({nums[lo], nums[i], nums[hi]});
+                    int tmpl = lo, tmph = hi;
+                    while (lo < i && nums[tmpl] == nums[lo]) lo++;
+                    while (hi > i && nums[tmph] == nums[hi]) hi--;
+                } else if (tmp >= 0) {
+                    hi--;
+                } else {
+                    lo++;
                 }
-                if (i != j)
-                    m[nums[j]]++;
             }
+            while (i+1 < n && nums[i+1] == nums[i]) i++;
         }
+        // sort(ans.begin(), ans.end());
+        // ans.erase(std::unique(ans.begin(), ans.end()), ans.end());
         return ans;
     }
 };
