8776950a7502adc4074370e69271fd1becf94915
Donald Tang <donald.tang.email@googlemail.com>
2024-05-15 15:54:03 +1000

    codeforces - 1828A-Divisible_Array.cpp

---
+++ codeforces/1828A-Divisible_Array.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAINRET(x) in##x
+#define what_is(x) cout << #x << " is " << x << endl;
+#define print_vec(x, n) for (int i = 0; i < n; i++) cout << x[i] << ' '; cout << endl;
+#define LL long long
+#define arr2 array<int,2>
+#define arr3 array<int,3>
+
+void solve();
+
+MAINRET(t) main(void) {
+    std::cin.tie(nullptr);
+    std::cin.sync_with_stdio(false);
+    int t; cin >> t;
+    while (t--)
+        solve();
+}
+
+constexpr int INF = (int)1e9 + 100; 
+constexpr LL LINF = std::numeric_limits<LL>::max() / 2;
+constexpr int NINF = -INF;
+constexpr int MX = 2 * 1e5 + 1;
+constexpr int MD = (int)1e9 + 7;
+
+int n, m, k;
+
+
+void solve() {
+    cin >> n;
+    vector<int> ans;
+    int sum = 0;
+    ans.push_back(0);
+    for (int i = 2; i <= n; i++) {
+        sum += i;
+        ans.push_back(i);
+    }
+    ans[0] = n + (n*(1+sum/n)-sum);
+    for (auto a : ans) cout << a << ' '; cout << '\n';
+}
+
+/*
+
+*/
+
+/*
+   Try this if you are stuck:
+1) binary search on answer
+2) Try solving it in reverse
+3) Think of a simpler problem 
+4) Think of elements which are special
+   (like minimum, maximum, deepest node in tree, root)
+5) Is it graph problem?
+    - super node? dp? cycles?
+6) sorting on something e.g. queries?
+7) Parity? something special
+8) is it monotonic? could you use a PQ/stack/queue?
+
+   EDGE CASES! N = 1, 2...
+   LONG LONG
+*/
--- prev.cpp
+++ prev.cpp
@@ -1,73 +1,27 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define MAINRET(x) in##x
-#define what_is(x) cout << #x << " is " << x << endl;
-#define print_vec(x, n) for (int i = 0; i < n; i++) cout << x[i] << ' '; cout << endl;
-#define LL long long
-#define arr2 array<int,2>
-#define arr3 array<int,3>
-
-void solve();
-
-MAINRET(t) main(void) {
-    std::cin.tie(nullptr);
-    std::cin.sync_with_stdio(false);
-    LL t;
-    cin >> t;
-    while (t--)
-        solve();
-}
-
-constexpr int INF = (int)1e9 + 100;
-constexpr LL LINF = std::numeric_limits<LL>::max() / 2;
-constexpr int NINF = -INF;
-constexpr int MX = 2 * 1e5 + 1;
-constexpr int MD = (int)1e9 + 7;
-
-int n, m, k;
-vector<arr2> adj[MX];
-int ans = 0;
-
-void dfs(int u, int reach, int ct, int par) {
-    ans = max(ans, ct);
-    for (auto &[v, i] : adj[u]) {
-        if (v == par) continue;
-        dfs(v, i, ct + (i <= reach), u);
+class TimeMap {
+public:
+    TimeMap() {
+        
     }
-}
-
-void solve() {
-    ans = 0;
-    cin >> n;
-    for (int i = 0; i < n; i++) adj[i].clear();
-    for (int i = 0; i < n-1; i++) {
-        int u, v; cin >> u >> v; u--; v--;
-        adj[u].push_back({v, i});
-        adj[v].push_back({u, i});
+    unordered_map<string, vector<int>> m;
+    unordered_map<string, vector<string>> c;
+    void set(string key, string value, int timestamp) {
+        m[key].push_back(timestamp);
+        c[key].push_back(std::move(value));
     }
-    dfs(0, -1, 1, -1);
-    cout << ans << '\n';
-}
+    
+    string get(string key, int timestamp) {
+        auto &vec = m[key];
+        auto it = upper_bound(vec.begin(), vec.end(), timestamp);
+        if (it == vec.begin()) return "";
+        it--;
+        return c[key][it-vec.begin()];
+    }
+};
 
-/*
-
-*/
-
-/*
-   Try this if you are stuck:
-1) binary search on answer
-2) Try solving it in reverse
-3) Think of a simpler problem 
-4) Think of elements which are special
-   (like minimum, maximum, deepest node in tree, root)
-5) Is it graph problem?
-    - super node? dp? cycles?
-6) sorting on something e.g. queries?
-7) Parity? something special
-8) is it monotonic? could you use a PQ/stack/queue?
-
-   EDGE CASES! N = 1, 2...
-   LONG LONG
-*/
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
