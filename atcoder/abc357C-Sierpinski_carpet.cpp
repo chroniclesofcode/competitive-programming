6c9c2fb8c54ea9b54d7a3b4b750a2332c8098be3
Donald Tang <donald.tang.email@googlemail.com>
2024-06-09 10:12:15 +1000

    atcoder - abc357C-Sierpinski_carpet.cpp

--- atcoder/abc357C-Sierpinski_carpet.cpp
+++ atcoder/abc357C-Sierpinski_carpet.cpp
@@ -25,168 +25,35 @@ constexpr int MX = 2 * 1e5 + 1;
 constexpr int MD = (int)1e9 + 7;
 
 int n, m, k;
-
-// Range update, Range query -> can  be used in most applications
-class SegmentTree {
-private:
-    int n;
-    vector<long long> segtree;
-    vector<long long> lazy;
-public:
-    SegmentTree() {}
-
-    SegmentTree(int sz) : n{sz} {
-        segtree.resize(1 + 4 * sz);
-        lazy.resize(1 + 4 * sz);
-    }
-
-    void build(int node, int L, int R, vector<int> &v) {
-        if (L == R) {
-            segtree[node] = v[L];
-            return;
-        }
-        int mid = (L + R) / 2;
-        build(node*2, L, mid, v);
-        build(node*2+1, mid+1, R, v);
-        segtree[node] = segtree[node*2] + segtree[node*2+1];
-    }
-
-    void lz(int node, int L, int R) {
-        segtree[node] += lazy[node] * (R - L + 1);
-        if (L != R) {
-            lazy[node*2] += lazy[node];
-            lazy[node*2+1] += lazy[node];
-        }
-        lazy[node] = 0;
-    }
-
-    void update(int node, int L, int R, int Lq, int Rq, int val) {
-        if (lazy[node])
-            lz(node, L, R);
-        if (R < Lq || L > Rq)
-            return;
-        if (Lq <= L && R <= Rq) {
-            lazy[node] = val;
-            lz(node, L, R);
-            return;
-        }
-        int mid = (L + R) / 2;
-        update(node*2, L, mid, Lq, Rq, val);
-        update(node*2+1, mid+1, R, Lq, Rq, val);
-        segtree[node] = segtree[node*2] + segtree[node*2+1];
-    }
-
-    long long query(int node, int L, int R, int Lq, int Rq) {
-        if (lazy[node])
-            lz(node, L, R);
-        if (R < Lq || L > Rq)
-            return 0;
-        if (Lq <= L && R <= Rq)
-            return segtree[node];
-        int mid = (L + R) / 2;
-        return query(node*2, L, mid, Lq, Rq) + query(node*2+1, mid+1, R, Lq, Rq);
-    }
-
-    void op1(int l, int r, int x) {
-    }
-
-    void op2(int l, int r, int x) {
-    }
-};
-
-
-
-// Range update, Range query -> can  be used in most applications
-class Ans {
-private:
-    int n;
-    vector<long long> segtree;
-    vector<long long> lazy;
-    SegmentTree a, b;
-public:
-    Ans(int sz, vector<int> &av, vector<int> &bv) : n{sz} {
-        segtree.resize(1 + 4 * sz);
-        lazy.resize(1 + 4 * sz);
-        a = SegmentTree(sz);
-        b = SegmentTree(sz);
-        a.build(1, 0, sz-1, av);
-        b.build(1, 0, sz-1, bv);
-    }
-
-    void build(int node, int L, int R, vector<int> &v) {
-        if (L == R) {
-            segtree[node] = v[L];
-            return;
-        }
-        int mid = (L + R) / 2;
-        build(node*2, L, mid, v);
-        build(node*2+1, mid+1, R, v);
-        segtree[node] = segtree[node*2] + segtree[node*2+1];
-    }
-
-    void lz(int node, int L, int R) {
-        segtree[node] += lazy[node] * (R - L + 1);
-        if (L != R) {
-            lazy[node*2] += lazy[node];
-            lazy[node*2+1] += lazy[node];
-        }
-        lazy[node] = 0;
-    }
-
-    void update(int node, int L, int R, int Lq, int Rq, int val) {
-        if (lazy[node])
-            lz(node, L, R);
-        if (R < Lq || L > Rq)
-            return;
-        if (Lq <= L && R <= Rq) {
-            lazy[node] = val;
-            lz(node, L, R);
-            return;
-        }
-        int mid = (L + R) / 2;
-        update(node*2, L, mid, Lq, Rq, val);
-        update(node*2+1, mid+1, R, Lq, Rq, val);
-        segtree[node] = segtree[node*2] + segtree[node*2+1];
-    }
-
-    long long query(int node, int L, int R, int Lq, int Rq) {
-        if (lazy[node])
-            lz(node, L, R);
-        if (R < Lq || L > Rq)
-            return 0;
-        if (Lq <= L && R <= Rq)
-            return segtree[node];
-        int mid = (L + R) / 2;
-        return query(node*2, L, mid, Lq, Rq) + query(node*2+1, mid+1, R, Lq, Rq);
-    }
-};
-
-
-
+char ans[8][740][740];
 
 void solve() {
-    int q;
-    cin >> n >> q;
-    vector<int> a(n), b(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    for (int i = 0; i < n; i++) {
-        cin >> b[i];
-    }
-    Ans s(n, a, b);
-    for (int i = 0; i < q; i++) {
-        int op, l, r, x;
-        if (op == 1) {
-            cin >> l >> r >> x; l--; r--;
-            a.op1(l, r, x);
-        } else if (op == 2) {
-            cin >> l >> r >> x; l--; r--;
-            a.op2(l, r, x);
-        } else {
-            cin >> l >> r;
-            cout << a.query(l, r) << '\n';
+    cin >> n;
+    ans[0][0][0] = '#';
+    int pw = 1;
+    for (int l = 1; l <= n; l++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < pw; k++) {
+                    for (int m = 0; m < pw; m++) {
+                        if (i == 1 && j == 1) {
+                            ans[l][i*pw+k][j*pw+m] = '.';
+                        } else {
+                            ans[l][i*pw+k][j*pw+m] = ans[l-1][k][m];
+                        }
+                    }
+                }
+            }
         }
+        pw *= 3;
+    }
+    int lvl = 1;
+    for (int i = 0; i < n; i++) lvl *= 3;
+    for (int i = 0; i < lvl; i++) {
+        for (int j = 0; j < lvl; j++) {
+            cout << ans[n][i][j];
+        }
+        cout << '\n';
     }
 }
 
@@ -210,3 +77,5 @@ void solve() {
    EDGE CASES! N = 1, 2...
    LONG LONG
 */
+
+
--- prev.cpp
+++ prev.cpp
@@ -1,105 +1,14 @@
-#include <bits/stdc++.h>
 
-using namespace std;
+n = int(input())
+md = 998244353
+numdigits = 0
+tmp = n
+while (tmp > 0):
+    numdigits += 1
+    tmp //= 10
+    
+ans = n * (pow(10, n * numdigits, md)-1) * pow(pow(10, numdigits)-1, md-2, md)
+ans %= md
 
-#define MAINRET(x) in##x
-#define LL long long
-
-void solve();
-
-MAINRET(t) main(void) {
-    std::cin.tie(nullptr);
-    std::cin.sync_with_stdio(false);
-
-        solve();
-}
-
-#define INF numeric_limits<LL>::max() / 2
-#define NINF -INF
-
-const LL MX = 3 * 1e5;
-//const LL MOD = 1e7;
-
-LL n, q;
-LL par[MX], grp[MX], tin[MX], tout[MX], depth[MX], comp[MX], pos[MX], sz[MX];
-bool vis[MX];
-vector<LL> adj[MX], cycle;
-LL timer = 0;
-
-void dfs(LL u) {
-    tin[u] = ++timer;
-    for (LL v : adj[u]) {
-        // Since we processed the full cycle before, this
-        // makes sure we're not processing an element in
-        // the cycle, and just keeping to tree nodes.
-        if (comp[v] == v) continue;
-        depth[v] = depth[u]+1;
-        grp[v] = grp[u];
-        comp[v] = comp[u];
-        dfs(v);
-    }
-    tout[u] = ++timer;
-}
-
-void solve() {
-    cin >> n;
-    for (LL i = 0; i < n; i++) {
-        cin >> par[i]; 
-        par[i]--;
-        adj[par[i]].push_back(i);
-    }
-
-    // Initialisations
-    for (LL i = 0; i < n; i++) {
-        comp[i] = -1;
-    }
-
-    // Find cycle
-    for (LL i = 0; i < n; i++) {
-        //cout << i << " comp[i] " << comp[i] << endl;
-        if (comp[i] != -1) {
-            continue;
-        }
-        LL u = i;
-        // Arrives at cycle
-        while (!vis[u]) {
-            vis[u] = 1;
-            u = par[u];
-        }
-        LL cycs = u;
-        LL ct = 1;
-        // Iterates through cycle again, setting values
-        while (u != cycs || cycle.empty()) {
-            cycle.push_back(u);
-            grp[u] = i;
-            pos[u] = ct++;
-            comp[u] = u; // Note: this holds true if in cycle
-            u = par[u];
-        }
-        LL cycle_size = cycle.size();
-        for (auto &c : cycle) {
-            timer = 0;
-            sz[c] = cycle_size;
-            // dfs outwards from each node to the end of 
-            // tree, propagating values throughout
-            dfs(c);
-        }
-        
-        cycle.clear();
-    }
-    int ret;
-    LL ans  = 0;
-    for (int i = 0; i < n; i++) {
-        if (comp[i] == i) {
-            ans += sz[i];
-        } else {
-            ans += depth[i] + sz[comp[i]];
-        }
-    }
-    cout << ans << '\n';
-}
-
-
-
-
+print(ans)
 
