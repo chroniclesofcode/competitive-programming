b18835e6160f6ab6cbcf7ecf5cc14d9bc31d762d
Donald Tang <donald.tang.email@googlemail.com>
2024-06-20 20:43:39 +1000

    cses - Distinct_Colors.cpp

---
+++ cses/Distinct_Colors.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAINRET(x) in##x
+#define what_is(x) cout << #x << " is " << x << endl;
+#define prLL_vec(x, n) for (LL i = 0; i < n; i++) cout << x[i] << ' '; cout << endl;
+#define LL long long
+#define arr2 array<LL,2>
+#define arr3 array<LL,3>
+
+void solve();
+
+MAINRET(t) main(void) {
+    std::cin.tie(nullptr);
+    std::cin.sync_with_stdio(false);
+
+        solve();
+}
+
+constexpr LL INF = (LL)1e9 + 100; 
+constexpr LL LINF = std::numeric_limits<LL>::max() / 2;
+constexpr LL NINF = -INF;
+constexpr LL MX = 2 * 1e5 + 1;
+constexpr LL MD = (LL)1e9 + 7;
+
+LL n, m, k;
+vector<LL> adj[MX];
+LL val[MX], ans[MX];
+
+set<int> dfs(int u, int p) {
+    set<int> s;
+    s.insert(val[u]);
+    for (int v : adj[u]) {
+        if (v == p) continue;
+        set<int> tmp = dfs(v, u);
+        if (tmp.size() > s.size()) swap(s, tmp);
+        s.merge(tmp);
+    }
+    ans[u] = s.size();
+    return s;
+}
+
+void solve() {
+    cin >> n; 
+    for (LL i = 0; i < n; i++) {
+        cin >> val[i];
+    }
+    for (LL i = 0; i < n-1; i++) {
+        LL a, b; cin >> a >> b; a--; b--;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    dfs(0, -1);
+    for (int i = 0; i < n; i++) cout << ans[i] << ' '; cout << '\n';
+}
+
+/*
+
+*/
+
+/*
+   Try this if you are stuck:
+1) binary search on answer
+2) Try solving it in reverse
+3) Think of a simpler problem 
+4) Think of elements which are special
+   (like minimum, maximum, deepest node in tree, root)
+5) Is it graph problem?
+    - super node? dp? cycles?
+6) sorting on something e.g. queries?
+7) Parity? something special
+8) is it monotonic? could you use a PQ/stack/queue?
+
+   EDGE CASES! N = 1, 2...
+   LONG LONG
+*/
