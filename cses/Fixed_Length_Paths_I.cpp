003978042be628c834b563edeab16ba72d98fcfb
Donald Tang <donald.tang.email@googlemail.com>
2024-06-27 16:52:10 +1000

    cses - Fixed_Length_Paths_I.cpp

--- cses/Fixed_Length_Paths_I.cpp
+++ cses/Fixed_Length_Paths_I.cpp
@@ -21,51 +21,71 @@ MAINRET(t) main(void) {
 constexpr LL INF = (LL)1e9 + 100; 
 constexpr LL LINF = std::numeric_limits<LL>::max() / 2;
 constexpr LL NINF = -INF;
-constexpr LL MX = 2 * 1e5 + 1;
+constexpr LL MX = 2 * 1e5 + 2;
 constexpr LL MD = (LL)1e9 + 7;
 
-LL n, m, k, sz[MX], rem[MX];
+LL n, m, k, sz[MX], rem[MX], ct[MX];
 vector<LL> adj[MX];
+LL ans = 0;
+LL mdep = 0;
 
-int get_subt(int u, int p) {
+LL get_subt(LL u, LL p) {
     sz[u] = 1;
-    for (int v : adj[u]) {
-        if (v == p) continue;
-        get_subt(v, u);
-        sz[u] += sz[v];
+    for (LL v : adj[u]) {
+        if (v == p || rem[v]) continue;
+        sz[u] += get_subt(v, u);
     }
     return sz[u];
 }
 
-int centroid(int u, int p, int tot) {
-    for (int v : adj[u]) {
+LL centroid(LL u, LL p, LL tot) {
+    for (LL v : adj[u]) {
         if (v == p || rem[v]) continue;
         if (sz[v] >= tot/2) return centroid(v, u, tot);
     }
     return u;
 }
+void calc(LL u, LL p, LL dep, bool add) {
+    if (dep > k) return;
+    mdep = max(mdep, dep);
+    if (add) {
+        ans += ct[k - dep];
+    } else {
+        ct[dep]++;
+    }
+    for (LL v : adj[u]) {
+        if (v == p || rem[v]) continue;
+        calc(v, u, dep+1, add);
+    }
+}
 
-void centroid_decomp(int node = 0) {
-    int c = centroid(node, -1, sz[node]);
+void centroid_decomp(LL node = 0) {
+    LL c = centroid(node, -1, get_subt(node, -1));
     rem[c] = true;
-    /*
-       Insert logic here
-    */
-    for (int v : adj[c]) {
+    
+    mdep = 0;
+    for (LL v : adj[c]) {
+        if (rem[v]) continue;
+        calc(v, c, 1, true);
+        calc(v, c, 1, false);
+    }
+    std::fill(ct+1, ct + mdep + 2, 0);
+
+    for (LL v : adj[c]) {
         if (rem[v]) continue;
         centroid_decomp(v);
     }
 }
 
 void solve() {
-    cin >> n; 
+    cin >> n >> k; 
     for (LL i = 0; i < n-1; i++) {
         LL a, b; cin >> a >> b; a--; b--;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    get_subt(0, -1);
+    ct[0] = 1;
     centroid_decomp(0);
+    cout << ans << '\n';
 }
-
 
--- prev.cpp
+++ prev.cpp
@@ -1,99 +1,51 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define MAINRET(x) in##x
-#define what_is(x) cout << #x << " is " << x << endl;
-#define prLL_vec(x, n) for (LL i = 0; i < n; i++) cout << x[i] << ' '; cout << endl;
-#define LL long long
-#define arr2 array<LL,2>
-#define arr3 array<LL,3>
-
-void solve();
-
-MAINRET(t) main(void) {
-    std::cin.tie(nullptr);
-    std::cin.sync_with_stdio(false);
-
-        solve();
-}
-
-constexpr LL INF = (LL)1e9 + 100; 
-constexpr LL LINF = std::numeric_limits<LL>::max() / 2;
-constexpr LL NINF = -INF;
-constexpr LL MX = 1001;
-constexpr LL MD = 998244353;
-
-LL n, m, k;
-unordered_map<string, LL> dp[MX];
-
-bool ispal(string &s) {
-    LL n = s.size();
-    for (LL i = 0; i < n; i++) {
-        if (s[i] != s[n-1-i]) return false;
-    }
-    return true;
-}
-
-void solve() {
-    cin >> n >> k;
-    string s; cin >> s;
-    if (k <= 1) { cout << 0 << '\n'; return; }
-    dp[0][""] = 1;
-    for (LL i = 1; i <= n; i++) {
-        for (auto &e : dp[i-1]) {
-            string str = e.first;
-            if (str.size() == k) str.erase(str.begin());
-            // not a wildcard
-            if (s[i-1] != '?') {
-                str.push_back(s[i-1]);
-                if (str.size() < k || !ispal(str)) {
-                    dp[i][str] += e.second;
-                    dp[i][str] %= MD;
-                }
-            } else {
-                str.push_back('A');
-                if (str.size() < k || !ispal(str)) {
-                    dp[i][str] += e.second;
-                    dp[i][str] %= MD;
-                }
-                str[str.size()-1] = 'B';
-                if (str.size() < k || !ispal(str)) {
-                    dp[i][str] += e.second;
-                    dp[i][str] %= MD;
+class Solution {
+public:
+    int n, m;
+    int dp[4][31][31];
+    vector<vector<int>> grid;
+    int cover(int sti, int stj, int edi, int edj) {
+        if (sti >= n || stj >= m || edi < 0 || edj < 0) return (int)0;
+        int x1 = INT_MAX, y1 = INT_MAX;
+        int x2 = 0, y2 = 0;
+        int n = grid.size(), m = grid[0].size();
+        for (int i = sti; i <= edi; i++) {
+            for (int j = stj; j <= edj; j++) {
+                if (grid[i][j] == 1) {
+                    x1 = min(x1, i);
+                    y1 = min(y1, j);
+                    x2 = max(x2, i);
+                    y2 = max(y2, j);
                 }
             }
         }
+        if (x1 == INT_MAX) return 0;
+        return (abs(x1-x2)+1) * (abs(y1-y2)+1);
     }
-    LL ans = 0;
-    for (auto &e : dp[n]) {
-        ans += e.second;
-        ans %= MD;
+    void rotate() {
+        vector<vector<int>> rot(m, vector<int>(n));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                rot[j][n-1-i] = grid[i][j];
+            }
+        }
+        swap(n, m);
+        swap(grid, rot);
     }
-    cout << ans << '\n';
-}
-
-/*
-   dp[i][mask] = if the last elements at i is (mask)->A or B
-   how many good strings do we have in total? The mask is to
-   confirm that we do have a palindrome.
-
-   for all ??? case -> we
-*/
-
-/*
-   Try this if you are stuck:
-1) binary search on answer
-2) Try solving it in reverse
-3) Think of a simpler problem 
-4) Think of elements which are special
-   (like minimum, maximum, deepest node in tree, root)
-5) Is it graph problem?
-    - super node? dp? cycles?
-6) sorting on something e.g. queries?
-7) Parity? something special
-8) is it monotonic? could you use a PQ/stack/queue?
-
-   EDGE CASES! N = 1, 2...
-   LONG LONG
-*/
+    int minimumSum(vector<vector<int>>& _grid) {
+        grid = std::move(_grid);
+        int ans = (int)1e9 + 7;
+        n = grid.size(), m = grid[0].size();
+        for (int ct = 0; ct < 4; ct++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = i; j < n; j++) {
+                    ans = min(ans, cover(0,0,i,m-1)+cover(i+1,0,j,m-1)+cover(j+1,0,n-1,m-1));
+                }
+                for (int j = 0; j < m; j++) {
+                    ans = min(ans, cover(0,0,i,j)+cover(0,j+1,i,m-1)+cover(i+1,0,n-1,m-1));
+                }
+            }
+            rotate();
+        }
+        return ans;
+    }
+};
