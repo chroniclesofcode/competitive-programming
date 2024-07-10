3a8056bd7c168bf546da0df17eb89271eabf1c01
Donald Tang <donald.tang.email@googlemail.com>
2024-07-10 15:38:09 +1000

    cses - Finding_Periods.cpp

---
+++ cses/Finding_Periods.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAINRET(x) in##x
+#define what_is(x) cout << #x << " is " << x << endl;
+#define LL long long
+#define arr array<int,2>
+
+void solve();
+
+MAINRET(t) main(void) {
+    std::cin.tie(nullptr);
+    std::cin.sync_with_stdio(false);
+
+        solve();
+}
+
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr int NINF = -INF;
+constexpr LL MX = 3 * 1e5;
+constexpr int MD = (int)1e9 + 7;
+
+int n, m, k;
+
+struct hashes {
+    int base, inv, mod, sz;
+    vector<long long> powers = {1}, invpowers = {1}, psa = {0};
+
+    hashes(string s, int m = 1000000007, int b = 131) {
+        sz = s.size();
+        base = b;
+        mod = m;
+
+        inv = 1;
+        long long cur = base;
+        int exp = m - 2;
+        for (int exp=m-2; exp; exp>>=1) {
+            if (exp&1) inv = inv * cur % m;
+            cur = cur * cur % m;
+        }
+
+        for (int i=0; i<s.size(); i++) {
+            powers.push_back(powers.back()*base%mod);
+            invpowers.push_back(invpowers.back()*inv%mod);
+            psa.push_back((psa.back()+s[i]*powers[i])%mod);
+        }
+    }
+
+    hashes() {}
+    
+    long long get(int l, int r) {
+        return (psa[r+1]-psa[l]+mod)*invpowers[l]%mod;
+    }
+};
+
+void solve() {
+    string s;
+    cin >> s;
+    n = s.size();
+
+    hashes h(s);
+    for (int i = 1; i <= n; i++) {
+        int curr = 0;
+        bool flag = true;
+        while (curr < n) {
+            int len = min(i-1, n-1-curr);
+            if (h.get(0, len) != h.get(curr,curr+len)) {
+                flag = false;
+                break;
+            }
+            curr += i;
+        }
+        if (flag) {
+            cout << i << ' ';
+        }
+    }
+    cout << '\n';
+}
+
+/*
+
+*/
+
+
+
--- prev.cpp
+++ prev.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 #define MAINRET(x) in##x
 #define what_is(x) cout << #x << " is " << x << endl;
-#define prt_vec(x, n) for (LL i = 0; i < n; i++) cout << x[i] << ' '; cout << endl;
+#define print_vec(x, n) for (int i = 0; i < n; i++) cout << x[i] << ' '; cout << endl;
 #define LL long long
-#define arr2 array<LL,2>
-#define arr3 array<LL,3>
+#define arr2 array<int,2>
+#define arr3 array<int,3>
 
 void solve();
 
@@ -18,142 +18,36 @@ MAINRET(t) main(void) {
         solve();
 }
 
-constexpr LL INF = (LL)1e9 + 100; 
+constexpr int INF = (int)1e9 + 100; 
 constexpr LL LINF = std::numeric_limits<LL>::max() / 2;
-constexpr LL NINF = -INF;
-constexpr LL MX = 2 * 1e5 + 1;
-constexpr LL MD = (LL)1e9 + 7;
+constexpr int NINF = -INF;
+constexpr int MX = 2 * 1e5 + 1;
+constexpr int MD = (int)1e9 + 7;
 
-int n, m, k, sz[MX], rem[MX], activated[MX];
-vector<int> adj[MX];
-vector<pair<int,int>> anc[MX]; // { ancestor, distance }
-struct {
-    int h1 = 0, h2 = 0, hchild = 0;
-} ans[MX];
+int n, m, k;
 
-int get_subt(LL u, LL p) {
-    sz[u] = 1;
-    for (LL v : adj[u]) {
-        if (v == p || rem[v]) continue;
-        sz[u] += get_subt(v, u);
-    }
-    return sz[u];
-}
-
-int centroid(LL u, LL p, LL tot) {
-    for (LL v : adj[u]) {
-        if (v == p || rem[v]) continue;
-        if (sz[v] > tot/2) return centroid(v, u, tot);
-    }
-    return u;
-}
-
-void process_subt(LL u, LL p, LL c, LL dep) {
-    for (LL v : adj[u]) {
-        if (v == p || rem[v]) continue;
-        dep++;
-        process_subt(v, u, c, dep);
-        dep--;
-    }
-    anc[u].push_back({c, dep});
-}
-
-void centroid_decomp(LL node = 0) {
-    LL c = centroid(node, -1, get_subt(node, -1));
-    rem[c] = true;
-    
-    // Add logic here
-    for (LL v : adj[c]) {
-        if (rem[v]) continue;
-        process_subt(v, c, c, 1);
-    }
-
-    for (LL v : adj[c]) {
-        if (rem[v]) continue;
-        centroid_decomp(v);
-    }
-}
-
-
-void update(int u) {
-    int prev = u;
-    activated[u] = 1;
-    for (int i = anc[u].size()-1; i >= 0; i--) {
-        auto &[a, dep] = anc[u][i];
-        auto &data = ans[a];
-        if (dep > data.h1) {
-            if (prev != data.hchild) {
-                data.h2 = data.h1;
-            }
-            data.h1 = dep;
-            data.hchild = prev;
-        } else if (prev != data.hchild && dep > data.h2) {
-            data.h2 = dep;
-        }
-        prev = a;
-    }
-}
-
-int query(int u) {
-    int prev = u;
-    int ret = ans[u].h1;
-    for (int i = anc[u].size()-1; i >= 0; i--) {
-        auto &[a, dep] = anc[u][i];
-        auto &data = ans[a];
-        if (!activated[a]) {
-            prev = a;
-            continue;
-        }
-        if (data.hchild != prev) {
-            // if it's a diff subtree, you can do u
-            // to centroid, centroid to height
-            ret = max(ret, dep + data.h1);
-        } else {
-            // else to second highest...
-            ret = max(ret, dep + data.h2);
-        }
-        prev = a;
-    }
-    return ret;
-}
 
 void solve() {
-    ifstream cin("newbarn.in"); ofstream cout("newbarn.out");
-    cin >> n; 
-    vector<arr2> qs;
-    int curr = 1;
-    vector<int> roots;
-    for (int i = 0; i < n; i++) {
-        char op; int b;
-        cin >> op >> b;
-        if (op == 'B') {
-            qs.push_back({1, b});
-            if (b != -1) {
-                adj[b].push_back(curr);
-                adj[curr].push_back(b);
-            } else {
-                roots.push_back(curr);
-            }
-            curr++;
-        } else if (op == 'Q') {
-            qs.push_back({0, b});
-        }
-    }
-    for (auto r : roots) {
-        centroid_decomp(r);
-    }
-    curr = 1;
-    for (auto &cmd : qs) {
-        
-        if (cmd[0] == 1) {
-            // build
-            update(curr);
-            curr++;
-        } else {
-            // query
-            cout << query(cmd[1]) << '\n';
-        }
-    }
+
 }
 
+/*
 
+*/
+
+/*
+   Try this if you are stuck:
+1) binary search on answer
+2) Try solving it in reverse
+3) Think of a simpler problem 
+4) Think of elements which are special
+   (like minimum, maximum, deepest node in tree, root)
+5) Is it graph problem?
+    - super node? dp? cycles?
+6) sorting on something e.g. queries?
+7) Parity? something special
+8) is it monotonic? could you use a PQ/stack/queue?
+
+   EDGE CASES! N = 1, 2...
+   LONG LONG
+*/
