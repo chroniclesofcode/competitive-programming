7799a4123074f27b902fba439337d913ee6fe9d7
Donald Tang <donald.tang.email@googlemail.com>
2024-11-07 15:46:09 +1100

    leetcode - 155-Min_Stack.cpp

---
+++ leetcode/155-Min_Stack.cpp
@@ -0,0 +1,33 @@
+class MinStack {
+public:
+    stack<int> s, ms;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        s.push(val);
+        if (ms.empty() || val <= ms.top()) ms.push(val);
+    }
+    
+    void pop() {
+        if (!s.empty() && s.top() == ms.top()) ms.pop();
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return ms.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
