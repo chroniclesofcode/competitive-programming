9fd9279da4fb2cef2a70b870ee6b00bf77c23045
Donald Tang <donald.tang.email@googlemail.com>
2025-02-12 12:43:47 +1100

    leetcode - 502-IPO.cpp

--- leetcode/502-IPO.cpp
+++ leetcode/502-IPO.cpp
@@ -6,40 +6,34 @@ public:
         int n = profits.size();
         k = min(k, n);
         for (int i = 0; i < n; i++) {
-            diffs.push_back({profits[i], capital[i]});
+            diffs.push_back({capital[i], profits[i]});
         }
-        sort(diffs.begin(), diffs.end(), greater<arr2>());
-        priority_queue<arr2, vector<arr2>, greater<arr2>> pq;
-        int curr = w;
+        sort(diffs.begin(), diffs.end());
+        priority_queue<int> pq;
         int tot = 0;
-
-        auto process_pq = [&]() {
-            vector<arr2> tmp;
-            while (!pq.empty() && pq.top()[0] <= curr) {
-                auto tp = pq.top();
-                tmp.push_back({tp[1], tp[0]});
-                pq.pop();
-            }
-            sort(tmp.begin(), tmp.end(), greater<arr2>());
-            int add = min((int)tmp.size(), k - tot);
-            for (int j = 0; j < add; j++) {
-                curr += tmp[j][0];
-            }
-            tot += add;
-        };
-        
+        int curr = w;
         for (int i = 0; i < n; i++) {
-            if (diffs[i][0] < 0) break;
-            if (curr < diffs[i][1]) {
-                pq.push({diffs[i][1], diffs[i][0]});
+            auto& [cap, prof] = diffs[i];
+            if (cap <= curr) {
+                pq.push(prof);
                 continue;
             }
-            curr += diffs[i][0];
-            tot++;
-            process_pq();
-            if (tot == k) break;
+            while (!pq.empty() && curr < cap && tot < k) {
+                curr += pq.top();
+                tot++;
+                pq.pop();
+            }
+            if (curr < cap) {
+                break;
+            } else {
+                pq.push(prof);
+            }
         }
-        process_pq();
+        while (!pq.empty() && tot < k) {
+            curr += pq.top();
+            tot++;
+            pq.pop();
+        }
         return curr;
     }
 };
