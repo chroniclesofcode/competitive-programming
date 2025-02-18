29f1ffda9628f8f66130f2affb161a5dccbb69b6
Donald Tang <donald.tang.email@googlemail.com>
2025-02-19 10:43:59 +1100

    codeforces - 2064A-Brogramming_Contest

--- codeforces/2064A-Brogramming_Contest.cpp
+++ codeforces/2064A-Brogramming_Contest.cpp
@@ -14,11 +14,13 @@ void solve();
 MAINRET(t) main(void) {
     std::cin.tie(nullptr);
     std::cin.sync_with_stdio(false);
-
+    LL t;
+    cin >> t;
+    while (t--)
         solve();
 }
 
-constexpr int INF = (int)1e9 + 100; 
+constexpr int INF = (int)1e9 + 100;
 constexpr LL LINF = std::numeric_limits<LL>::max() / 2;
 constexpr int NINF = -INF;
 constexpr int MX = 2 * 1e5 + 1;
@@ -26,9 +28,23 @@ constexpr int MD = (int)1e9 + 7;
 
 int n, m, k, q;
 
-
 void solve() {
-
+    cin >> n;
+    string s;
+    cin >> s;
+    int zero = true;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '0') {
+            zero = true;
+        } else if (s[i] == '1') {
+            if (zero) ans++;
+            zero = false;
+        }
+    }
+    ans *= 2;
+    if (s[n-1] == '1') ans--;
+    cout << ans << '\n';
 }
 
 /*
