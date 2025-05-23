b76f9bcd265407033a14bc1fcc4b7c069dc8a24f
chroniclesofcode <donald.tang.email@gmail.com>
2025-05-23 14:59:04 +1000

    codeforces - 2094G-Chimpanzini_Bananini.cpp

--- prev.cpp
+++ prev.cpp
@@ -14,19 +14,18 @@ void solve();
 MAINRET(t) main(void) {
     std::cin.tie(nullptr);
     std::cin.sync_with_stdio(false);
-    LL t;
-    cin >> t;
-    while (t--)
+
         solve();
 }
 
-constexpr int INF = (int)1e9 + 100;
+constexpr int INF = (int)1e9 + 100; 
 constexpr LL LINF = std::numeric_limits<LL>::max() / 2;
 constexpr int NINF = -INF;
 constexpr int MX = 2 * 1e5 + 1;
 constexpr int MD = (int)1e9 + 7;
 
 int n, m, k, q;
+
 
 void solve() {
 
